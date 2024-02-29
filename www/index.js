daa38211940c8b3920267a8972ff9ff005ea4f47
nour-it <reply.nour.it@gmail.com>
2024-02-29 10:57:37 +0000

    (feat) add redis + elasticsearch

--- www/index.js
+++ www/index.js
@@ -1,1 +1,11 @@
-console.log("Hello")
+import { createClient } from 'redis';
+
+const conf = {
+  url: 'redis://localhost:6379'
+}
+const client = createClient(conf);
+client.on('error', err => console.log('Redis Client Error', err));
+await client.connect();
+await client.set('key', 'demo');
+const value = await client.get('key');
+console.log(value);
